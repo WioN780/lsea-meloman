c286c956d6450bd5514a97754ff062d6609f22b1
Ibai Aguirre <s206614@student.pg.edu.pl>
2025-03-26 15:36:08 +0100

    Added comments following javadoc.

--- src/main/java/com/meloman/project/data_model/Track.java
+++ src/main/java/com/meloman/project/data_model/Track.java
@@ -4,26 +4,62 @@ import lombok.Getter;
 import lombok.Setter;
 import java.util.Set;
 
+/**
+ * Represents a music track, extending the MediaItem class.
+ * Includes additional metadata such as duration.
+ */
 @Getter
 @Setter
 public class Track extends MediaItem implements Cloneable {
+
+    /**
+     * Duration of the track in minutes.
+     */
     private double duration;
 
-    public Track(String id, String title, int year, Set<String> genres, Set<String> styles, Artist artist, Label label, double duration) {
+    /**
+     * Constructs a new Track instance with the specified attributes.
+     *
+     * @param id       unique identifier of the track
+     * @param title    title of the track
+     * @param year     release year of the track
+     * @param genres   set of genres associated with the track
+     * @param styles   set of musical styles of the track
+     * @param artist   the artist associated with the track
+     * @param label    the label that produced or published the track
+     * @param duration the duration of the track in minutes
+     */
+    public Track(String id, String title, int year, Set<String> genres, Set<String> styles,
+                 Artist artist, Label label, double duration) {
         super(id, title, year, genres, styles, artist, label);
         this.duration = duration;
     }
 
+    /**
+     * Creates and returns a shallow clone of this Track.
+     * Uses the cloning mechanism from the superclass.
+     *
+     * @return a clone of this Track
+     */
     @Override
     public Track clone() {
         return (Track) super.clone();
     }
 
+    /**
+     * Displays basic information about the track to the console.
+     * Includes the title and duration.
+     */
     @Override
     public void displayInfo() {
         System.out.println("Track: " + getTitle() + " (" + duration + " mins)");
     }
 
+    /**
+     * Returns a string representation of the Track object.
+     *
+     * @return a string containing the track ID, title, and duration
+     */
     @Override
     public String toString() {
         return "Track [id=" + getId() + ", title=" + getTitle() + ", duration=" + duration + "]";
