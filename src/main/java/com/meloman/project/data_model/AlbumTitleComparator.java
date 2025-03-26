7d7b98664bc66a20760e808f5fcf3032ec87ab1e
p1345740 <s192066@student.pg.edu.pl>
2025-03-26 15:12:29 +0100

    error solved

--- src/main/java/com/meloman/project/data_model/AlbumTitleComparator.java
+++ src/main/java/com/meloman/project/data_model/AlbumTitleComparator.java
@@ -2,7 +2,22 @@ package com.meloman.project.data_model;
 
 import java.util.Comparator;
 
+/**
+ * A comparator to compare two albums based on their titles.
+ * This is used for sorting albums alphabetically by their title in ascending order.
+ *
+ */
+
 public class AlbumTitleComparator implements Comparator<Album> {
+
+    /**
+     * Compares two albums by their title in alphabetical order.
+     *
+     * @param a1 The first album to be compared.
+     * @param a2 The second album to be compared.
+     * @return A negative number if the title of the first album is less than the second one,
+     *         zero if they are equal, and a positive number if the first album's title is greater.
+     */
 
     @Override
     public int compare(Album a1, Album a2) {
--- src/main/java/com/meloman/project/data_model/Label.java
+++ src/main/java/com/meloman/project/data_model/Label.java
@@ -6,12 +6,29 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Represents a music label, which may have sub-labels and contact information.
+ * Labels are used to manage and organize media items such as albums and tracks.
+ */
+
 @Getter
 @Setter
 public class Label extends MediaOwner {
     private String parentLabel;
     private Set<String> subLabels;
     private String contactInfo;
+
+    /**
+     * Constructs a new Label object with the specified details.
+     *
+     * @param id The unique identifier for the label.
+     * @param name The name of the label.
+     * @param parentLabel The parent label under which this label operates.
+     * @param subLabels A set of sub-labels under this label.
+     * @param ownedItems A list of media items owned by this label.
+     * @param urls A set of URLs related to this label (e.g., official website).
+     * @param contactInfo Contact information for the label (e.g., email or phone number).
+     */
 
     public Label(String id, String name, String parentLabel, Set<String> subLabels,
                  List<MediaItem> ownedItems, Set<String> urls, String contactInfo) {
@@ -21,6 +38,12 @@ public class Label extends MediaOwner {
         this.contactInfo = contactInfo;
     }
 
+    /**
+     * Creates a deep clone of the label, including cloning the sub-labels set.
+     *
+     * @return A new Label object that is a copy of the current label.
+     */
+
     @Override
     public Label clone() {
         Label cloned = (Label) super.clone();
@@ -29,6 +52,13 @@ public class Label extends MediaOwner {
         }
         return cloned;
     }
+
+    /**
+     * Returns a string representation of the label, including the label's ID, name,
+     * parent label, sub-labels, and contact information.
+     *
+     * @return A string that describes the label and its details.
+     */
 
     @Override
     public String toString() {
