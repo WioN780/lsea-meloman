e3b8b231bc1fe04d61843661b0b5dfed192e24a6
Ibai Aguirre <s206614@student.pg.edu.pl>
2025-04-02 10:11:23 +0200

    Added main method to test DataProcessing

---
+++ src/main/java/com/meloman/project/entry_points/ProcessingTest.java
@@ -0,0 +1,33 @@
+package com.meloman.project.entry_points;
+import com.meloman.project.data_model.Album;
+import com.meloman.project.data_model.DataLoader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class ProcessingTest {
+    public static void main(String[] args) throws IOException {
+
+            try {
+            DataLoader loader = new DataLoader();
+
+            InputStream inputStream = Main.class.getClassLoader()
+                    .getResourceAsStream("com/meloman/project/data/data.csv");
+
+            if (inputStream == null) {
+                throw new IOException("Artxiboa ez da aurkitu");
+            }
+
+            List<Album> albums = loader.loadAlbums(inputStream);
+            System.out.println(albums.size() + " Album kargatuta");
+
+        } catch (IOException e) {
+            System.err.println("Error:" + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+}
+
+
+// polymorphism can be shown by displaying info of mediaitem owned or liked by a user
