9c5f9f2416a03cba431669baf6eca5ef349847a9
Ibai Aguirre <s206614@student.pg.edu.pl>
2025-04-02 17:10:43 +0200

    Refactored and adjusted the ProcessingTest to accept new loaders.

--- src/main/java/com/meloman/project/entry_points/ProcessingTest.java
+++ src/main/java/com/meloman/project/entry_points/ProcessingTest.java
@@ -1,33 +1,115 @@
 package com.meloman.project.entry_points;
 import com.meloman.project.data_model.Album;
-import com.meloman.project.utils.DataLoader;
+import com.meloman.project.data_model.Playlist;
+import com.meloman.project.utils.DiscoGSLoader;
+import com.meloman.project.utils.DiscoGSLoaderBatches;
+import com.meloman.project.utils.SpotifyPlaylistLoader;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Scanner;
 
 public class ProcessingTest {
     public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select a test to run:");
+        System.out.println("1: Test PlaylistJsonLoader");
+        System.out.println("2: Test DiscoGSLoaderBatches");
+        System.out.println("3: Test DiscoGSLoader");
+        System.out.print("Enter your choice (1-3): ");
 
-            try {
-            DataLoader loader = new DataLoader();
+        int choice = 0;
+        try {
+            choice = scanner.nextInt();
+        } catch (Exception e) {
+            System.err.println("Invalid input. Please enter a number between 1 and 3.");
+            return;
+        }
 
-            InputStream inputStream = Main.class.getClassLoader()
-                    .getResourceAsStream("com/meloman/project/data/DiscoGSdata.csv");
+        // Test PlaylistJsonLoader
+        if (choice == 1) {
+            for (int j = 0; j < 11; j++) {
+                try {
+                    SpotifyPlaylistLoader loader = new SpotifyPlaylistLoader();
 
-            if (inputStream == null) {
-                throw new IOException("Artxiboa ez da aurkitu");
+                    String file = "com/meloman/project/data/mpd.slice." + 1000 * j + "-" + ((1000 * (j + 1)) - 1) + ".json";
+
+                    InputStream inputStream = SpotifyPlaylistLoader.class.getClassLoader()
+                            .getResourceAsStream(file);
+
+                    if (inputStream == null) {
+                        throw new IOException("File not found." + file);
+                    }
+
+                    List<Playlist> playlists = loader.loadPlaylists(inputStream);
+                    System.out.println(playlists.size() + " playlists loaded");
+
+                    // Print details of first few playlists if available
+                    int playlistsToPrint = Math.min(3, playlists.size());
+                    for (int i = 0; i < playlistsToPrint; i++) {
+                        System.out.println("\nPlaylist " + (i + 1) + " details:");
+                        System.out.println(playlists.get(i).toString());
+                        System.out.println("Number of tracks: " + playlists.get(i).getNumTracks());
+                        System.out.println("Number of albums: " + playlists.get(i).getNumAlbums());
+                        System.out.println("Number of artists: " + playlists.get(i).getNumArtists());
+                    }
+
+                } catch (IOException e) {
+                    System.err.println("Error: " + e.getMessage());
+                    e.printStackTrace();
+                }
             }
 
-            List<Album> albums = loader.loadAlbums(inputStream);
-            System.out.println(albums.size() + " Album kargatuta");
 
-        } catch (IOException e) {
-            System.err.println("Error:" + e.getMessage());
-            e.printStackTrace();
         }
+
+        // Test DiscoGSLoaderBatches
+        else if (choice == 2) {
+            try {
+                DiscoGSLoaderBatches loader = new DiscoGSLoaderBatches();
+
+                InputStream inputStream = SpotifyPlaylistLoader.class.getClassLoader()
+                        .getResourceAsStream("com/meloman/project/data/DiscoGSdata.csv");
+
+                if (inputStream == null) {
+                    throw new IOException("File not found: DiscoGSdata.csv");
+                }
+
+                List<Album> albums = loader.loadAlbums(inputStream);
+                System.out.println(albums.size() + " albums loaded");
+
+            } catch (IOException e) {
+                System.err.println("Error: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+
+        // Test DiscoGSLoader
+        else if (choice == 3) {
+            try {
+                DiscoGSLoader loader = new DiscoGSLoader();
+
+                InputStream inputStream = SpotifyPlaylistLoader.class.getClassLoader()
+                        .getResourceAsStream("com/meloman/project/data/DiscoGSdata.csv");
+
+                if (inputStream == null) {
+                    throw new IOException("File not found: DiscoGSdata.csv");
+                }
+
+                List<Album> albums = loader.loadAlbums(inputStream);
+                System.out.println(albums.size() + " albums loaded");
+
+            } catch (IOException e) {
+                System.err.println("Error: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+
+        else {
+            System.out.println("Invalid choice. Please run again and select a number between 1 and 3.");
+        }
+
+        scanner.close();
     }
-
 }
-
-
-// polymorphism can be shown by displaying info of mediaitem owned or liked by a user
