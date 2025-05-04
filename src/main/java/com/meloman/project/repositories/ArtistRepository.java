bc76def0b6d79d13d2291ca32619553f30b219a4
Ibai Aguirre <s206614@student.pg.edu.pl>
2025-05-04 20:42:51 +0200

    Fixed the correct dependency.

--- src/main/java/com/meloman/project/repositories/ArtistRepository.java
+++ src/main/java/com/meloman/project/repositories/ArtistRepository.java
@@ -1,6 +1,6 @@
 package com.meloman.project.repositories;
 
-import com.meloman.project.transaction_model.Artist;
+import com.meloman.project.database_model.Artist;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
