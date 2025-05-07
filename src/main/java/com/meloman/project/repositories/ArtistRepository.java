62a20712cf60b177499eb14985b92c635522fa33
Mark Slipenkyi <s196668@student.pg.edu.pl>
2025-05-07 14:06:50 +0200

    Added test queries

--- src/main/java/com/meloman/project/repositories/ArtistRepository.java
+++ src/main/java/com/meloman/project/repositories/ArtistRepository.java
@@ -2,10 +2,33 @@ package com.meloman.project.repositories;
 
 import com.meloman.project.database_model.Artist;
 
+import com.meloman.project.database_model.Track;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ArtistRepository extends JpaRepository<Artist, String> {
-    //
+    // Find artists by name
+    List<Artist> findByName(String name);
+
+    @Query(value = """
+    SELECT a.*
+    FROM Artist a
+    JOIN Track t ON a.id = t.artist_id
+    GROUP BY a.id
+    HAVING COUNT(*) = (
+        SELECT MAX(track_count)
+        FROM (
+            SELECT COUNT(*) AS track_count
+            FROM Track
+            GROUP BY artist_id
+        )
+    )
+    """, nativeQuery = true)
+    List<Artist> findWithMostTracks();
 }
--- src/main/java/com/meloman/project/repositories/LabelRepository.java
+++ src/main/java/com/meloman/project/repositories/LabelRepository.java
@@ -1,11 +1,34 @@
 package com.meloman.project.repositories;
 
+import com.meloman.project.database_model.Artist;
 import com.meloman.project.database_model.Label;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LabelRepository extends JpaRepository<Label, String> {
     //
+
+    // Find artists by name
+    List<Label> findByName(String name);
+
+    @Query(value = """
+    SELECT a.*
+    FROM Label l
+    JOIN Track t ON l.id = t.artist_id
+    GROUP BY l.id
+    HAVING COUNT(*) = (
+        SELECT MAX(track_count)
+        FROM (
+            SELECT COUNT(*) AS track_count
+            FROM Track
+            GROUP BY label_id
+        )
+    )
+    """, nativeQuery = true)
+    List<Label> findWithMostTracks();
 }
