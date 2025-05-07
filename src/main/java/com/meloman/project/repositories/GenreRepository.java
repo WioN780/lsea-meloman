eeda30f0e2c4a335f47bf8efb817f13878aba623
Mark Slipenkyi <s196668@student.pg.edu.pl>
2025-05-07 14:19:24 +0200

    Added more test queries

--- src/main/java/com/meloman/project/repositories/GenreRepository.java
+++ src/main/java/com/meloman/project/repositories/GenreRepository.java
@@ -2,10 +2,40 @@ package com.meloman.project.repositories;
 
 import com.meloman.project.database_model.Genre;
 
+import com.meloman.project.database_model.Style;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, String> {
     //
+    @Query(
+    value =
+            """
+            SELECT g.*
+                    FROM Genre g
+                    JOIN song_genre ss ON sg.genre_id = g.id
+                    JOIN album_genre alg ON alg.genre_id = s.id
+                    GROUP BY s.id
+                    HAVING COUNT(*) = (
+                        SELECT MAX(appearances_count)
+                        FROM (
+                            SELECT COUNT(*) AS appearances_count
+                            FROM (
+                                SELECT genre_id FROM song_genre
+                                UNION ALL
+                                SELECT genre_id FROM album_genre
+                            ) combined_genres
+                            GROUP BY genre_id
+                        )
+                    )
+                    FETCH FIRST :n ROWS ONLY
+            """, nativeQuery = true
+    )
+    List<Genre> findMostPopular(@Param("n") int n);
+
 }
--- src/main/java/com/meloman/project/repositories/StyleRepository.java
+++ src/main/java/com/meloman/project/repositories/StyleRepository.java
@@ -3,9 +3,38 @@ package com.meloman.project.repositories;
 import com.meloman.project.database_model.Style;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StyleRepository extends JpaRepository<Style, String> {
     //
+
+    @Query(
+    value =
+    """
+    SELECT s.*
+            FROM Style s
+            JOIN song_style ss ON ss.style_id = s.id
+            JOIN album_style als ON als.style_id = s.id
+            GROUP BY s.id
+            HAVING COUNT(*) = (
+                SELECT MAX(appearances_count)
+                FROM (
+                    SELECT COUNT(*) AS appearances_count
+                    FROM (
+                        SELECT style_id FROM song_style
+                        UNION ALL
+                        SELECT style_id FROM album_style
+                    ) combined_styles
+                    GROUP BY style_id
+                )
+            )
+            FETCH FIRST :n ROWS ONLY
+    """, nativeQuery = true
+    )
+    List<Style> findMostPopular(@Param("n") int n);
 }
